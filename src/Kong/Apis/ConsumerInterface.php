7dff0ef8df45c0a5977a8426473099f2c3b964c0
therealgambo <git@fucking.nz>
2017-09-03 14:40:22 +0100

    Remove /cluster endpoints Add new methods for listing plugins assigned to consumers

--- src/Kong/Apis/ConsumerInterface.php
+++ src/Kong/Apis/ConsumerInterface.php
@@ -7,7 +7,9 @@ interface ConsumerInterface
     public function add(array $body = [], array $headers = []);
     public function delete($identifier, array $headers = []);
     public function get($identifier, array $params = [], array $headers = []);
+    public function getConsumerPlugin($identifier, $plugin_identifier, array $params = [], array $headers = []);
     public function list(array $params = [], array $headers = []);
+    public function listConsumerPlugins($identifier, array $params = [], array $headers = []);
     public function update($identifier, array $body = [], array $headers = []);
     public function updateOrCreate(array $body = [], array $headers = []);
 }
